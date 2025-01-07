8cec79a7524fd8580c99d79b96fe4cfac119375e
itsbppradhan <144833404+itsbppradhan@users.noreply.github.com>
2025-01-07 14:05:46 +0530

    Cache support

--- public/sw.js
+++ public/sw.js
@@ -3,9 +3,37 @@ self.addEventListener('install', (event) => {
     caches.open('b-meet-v1').then((cache) => {
       return cache.addAll([
         '/',
+        // Icons
         '/icons/logo.svg',
         '/icons/hamburger.svg',
-        // Add other static assets you want to cache
+        '/icons/add-meeting.svg',
+        '/icons/add-personal.svg',
+        '/icons/call-ended.svg',
+        '/icons/checked.svg',
+        '/icons/copy.svg',
+        '/icons/Home.svg',
+        '/icons/join-meeting.svg',
+        '/icons/loading-circle.svg',
+        '/icons/play.svg',
+        '/icons/previous.svg',
+        '/icons/recordings.svg',
+        '/icons/schedule.svg',
+        '/icons/share.svg',
+        '/icons/three-dots.svg',
+        '/icons/upcoming.svg',
+        '/icons/Video.svg',
+        '/icons/yoom-logo.svg',
+        // Images
+        '/images/avatar-1.jpeg',
+        '/images/avatar-2.jpeg',
+        '/images/avatar-3.png',
+        '/images/avatar-4.png',
+        '/images/avatar-5.png',
+        '/images/hero-background.png',
+        '/images/join-meeting.png',
+        '/images/meeting-created.png',
+        '/images/no-meetings.png',
+        '/images/no-recordings.png'
       ]);
     })
   );
