c1b322277263e7042ae3942adeca65e0ff3daf61
itsbppradhan <144833404+itsbppradhan@users.noreply.github.com>
2025-03-07 11:04:15 +0530

    Overlay title bar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -6,26 +6,28 @@ import { SignedIn, UserButton } from '@clerk/nextjs'
 
 const Navbar = () => {
   return (
-    <nav className='flex flex-between fixed z-50 w-full bg-dark-1 px-6
-    py-4 lg:px-10'>
-      <Link href='/' className="flex items-center gap-1">
-        <Image
-          src="/icons/logo.svg"
-          width={32}
-          height={32}
-          alt="BMeet Logo"
-          className='max-sm:size-10'
-          />
-        <p className='text-[26px] font-extrabold text-white max-sm:hidden'>B.Meet</p>
-      </Link>
-      <div className='flex flex-between gap-5'>
-        {/**clerk user management */}
-        <SignedIn>
-          <UserButton />
-        </SignedIn>
-        <MobileNav />
-      </div>
-    </nav>
+    <main className='bg-dark-1 fixed w-screen h-16 z-50'>
+      <nav className='flex flex-between fixed z-50 w-screen bg-dark-1 pl-6 pr-6 lg:pl-10 lg:pr-10' id="title-bar">
+        <Link href='/' className="flex items-center gap-1">
+          <Image
+            src="/icons/logo.svg"
+            width={32}
+            height={32}
+            alt="BMeet Logo"
+            className='max-sm:size-10'
+            />
+          <p className='text-[26px] font-extrabold text-white max-sm:hidden'>B.Meet</p>
+        </Link>
+        <div className='w-[calc(100%-200px)] px-4 h-16' id="drag-reg"/>
+        <div className='flex flex-between gap-5'>
+          {/**clerk user management */}
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
+          <MobileNav />
+        </div>
+      </nav>
+        </main>
   )
 }
 
